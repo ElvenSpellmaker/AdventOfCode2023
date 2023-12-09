9c2992a64312d627434a41182c18fddd7cba7eb4
Jack Blower <Jack@elvenspellmaker.co.uk>
2023-12-09 15:45:50 +0000

    feat: Day 9
     - Day 9
     - Day 8 Part 2 slight improvement

---
+++ d9p2.php
@@ -0,0 +1,30 @@
+<?php
+
+$file = explode("\n", rtrim(file_get_contents('d9.txt'), "\n"));
+
+function find_next_number(array $arr) : int
+{
+	$diffs = [];
+	$allZero = true;
+	for ($i = 1; $i < count($arr); $i++)
+	{
+		$diffElem = $arr[$i] - $arr[$i - 1];
+		$diffElem !== 0 && $allZero = false;
+		$diffs[] = $diffElem;
+	}
+
+	if ($allZero)
+	{
+		return end($arr);
+	}
+
+	return end($arr) + find_next_number($diffs);
+}
+
+$sum = 0;
+foreach ($file as $line)
+{
+	$sum += find_next_number(array_reverse(explode(' ', $line)));
+}
+
+echo $sum, "\n";
